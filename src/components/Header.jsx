c2229bb9df19857fe4a8f30eab1cd371486c0d93
Novoselets28 <artem28novoselets11@gmail.com>
2023-12-22 20:25:16 +0200

    created base structure for app

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/ru/thumb/7/78/Trollface.svg/1200px-Trollface.svg.png"
+        alt="#"
+        className="header--image"
+      />
+      <h2 className="header--title">Header component</h2>
+    </header>
+  );
+};
+
+export default Header;
